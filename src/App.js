bd94f9b0cf79709e20ae335b6ee6fa54c8043bb9
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-08-04 23:18:46 -0500

    added pages folder containing an aboutpage, contactpage, and homepage file. also imported navigation and repeating content components to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,1 +1,5 @@
 import './App.css';
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
