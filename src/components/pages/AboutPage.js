69208d7af256dc296274cc6a6b2778b61edc8d1e
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-08-04 23:25:06 -0500

    imported react as well as footer, header, and navigation components to contact and about pages. imported those same components as well as maincontent, navigation, and project components

---
+++ src/components/pages/AboutPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+import Header from "../Header";
+import Navigation from "../Navigation";
+import Footer from "../Footer";
---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import Header from "../Header";
+import Navigation from "../Navigation";
+import MainContent from "../MainContent";
+import Project from "../Project";
+import Footer from "../Footer";
