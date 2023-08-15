fd61d9cfc90854950db88524b6ad39aba30b6ab6
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-08-15 10:10:16 -0500

    resume page

---
+++ src/components/pages/ResumePage.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Header from "../components/Header";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+
+export default function ResumePage() {
+    <div>
+        <Header />
+        <Navigation />
+        <h1>Resume</h1>
+        <h3> Download my resume <a href="RESUME HERE"><span>here</span></a></h3>
+        <ul>
+            <h2>Front-end Proficiencies</h2>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>JQuery</li>
+            <li>Responsive Design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+            <li>Handlebars</li>
+            
+
+            <h2>Back-end Proficiencies</h2>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+            <li>Progressive Web Applications (PWA)</li>
+            <li>Webpack</li>
+            <li>SQL</li>
+            <li>NoSQL</li>
+        </ul>
+        <Footer />
+    </div>
+}
+
+
+
+
