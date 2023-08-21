90b81e3a9cb4545e8a32a8cc41b3438bb5b5c326
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-08-21 16:25:26 -0400

    finished navigation section

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,44 +3,51 @@ import React from "react";
 
 function Navigation({currentPage, handlePageChange}) {
     return (
-        <ul className="nav nav-tabs">
-            <li className="nav-item">
-                <a
-                    href="#home"
-                    onClick={() => handlePageChange("HomePage")}
-                    className={currentPage === "HomePage" ? "nav-link active" : "nav-link"}
-                >
-                    Home
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#about"
-                    onClick={() => handlePageChange("AboutPage")}
-                    className={currentPage === "AboutPage" ? "nav-link active" : "nav-link"}
-                >
-                    About
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#contact"
-                    onClick={() => handlePageChange("ContactPage")}
-                    className={currentPage === "ContactPage" ? "nav-link active" : "nav-link"}
-                >
-                    Contact
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#resume"
-                    onClick={() => handlePageChange("ResumePage")}
-                    className={currentPage === "ResumePage" ? "nav-link active" : "nav-link"}
-                >
-                    Resume
-                </a>
-            </li>
-        </ul>
+        <nav className="navbar navbar-expand-lg" id="navigation">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#home" onClick={() => handlePageChange("HomePage")}> <img src="./public/LC-HeadShot-MK-icon.png" alt="Headhshot of LC Sewell"></img></a>
+                <div className="collapse navbar-collapse" id="navbarHamburger">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a
+                                href="#home"
+                                onClick={() => handlePageChange("HomePage")}
+                                className={currentPage === "HomePage" ? "nav-link active" : "nav-link"}
+                            >
+                                Home
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                href="#about"
+                                onClick={() => handlePageChange("AboutPage")}
+                                className={currentPage === "AboutPage" ? "nav-link active" : "nav-link"}
+                            >
+                                About
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                href="#contact"
+                                onClick={() => handlePageChange("ContactPage")}
+                                className={currentPage === "ContactPage" ? "nav-link active" : "nav-link"}
+                            >
+                                Contact
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                href="#resume"
+                                onClick={() => handlePageChange("ResumePage")}
+                                className={currentPage === "ResumePage" ? "nav-link active" : "nav-link"}
+                            >
+                                Resume
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
