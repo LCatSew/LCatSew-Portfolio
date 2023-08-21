fba0fc5fad9932ead15ec4221375d555e9d0472e
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-08-21 14:48:46 -0400

    corrected navigation file

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,5 @@
-import React, { useState }  from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import MainContent from "./MainContent";
-import Project from "./Project";
+import React from "react";
+
 
 function Navigation({currentPage, handlePageChange}) {
     return (
