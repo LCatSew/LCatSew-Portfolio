d6034f3867aa83f7edc70bff892ed326fb17605f
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-08-21 16:29:45 -0400

    finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
-import React, { useState } from "react";
-import Header from "./Header";
-import Navigation from "./Navigation";
-import MainContent from "./PortfolioContainer";
-import Project from "./Project";
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="text-center">
+            <div classname="container p-3">Last updated Aug 2023</div>
+        </footer>
+    );
+}
+
+export default Footer;
